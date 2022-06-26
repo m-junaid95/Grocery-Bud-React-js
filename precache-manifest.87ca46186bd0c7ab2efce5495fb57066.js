f03db5cd76e03870b12b4b98cac3cabf432f9cce
Junaid <zulfiqar.junaid95@gmail.com>
2022-06-25 18:26:40 -0700

    Updates

---
+++ precache-manifest.87ca46186bd0c7ab2efce5495fb57066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d270df51263b81a9e01a8ce0ec632e13",
+    "url": "/Grocery-Bud-React-js/index.html"
+  },
+  {
+    "revision": "8fb7b11197bf47a4bf83",
+    "url": "/Grocery-Bud-React-js/static/css/main.35fd4e1c.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/Grocery-Bud-React-js/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Grocery-Bud-React-js/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb7b11197bf47a4bf83",
+    "url": "/Grocery-Bud-React-js/static/js/main.90c50ccc.chunk.js"
+  },
+  {
+    "revision": "1df64e19ae775a4f9203",
+    "url": "/Grocery-Bud-React-js/static/js/runtime-main.8336b45f.js"
+  }
+]);
